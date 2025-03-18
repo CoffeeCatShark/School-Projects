ce80f8549f5275a2ba8f58d556b3e22088e23e19
CoffeeStrike <omniexpansiontheory@gmail.com>
2025-03-18 21:19:22 +0800

    Add files via upload

--- Midterms Library System.cpp
+++ Midterms Library System.cpp
@@ -312,6 +312,7 @@ class Library{
 			case 4:{
 				cout<<"Enter New Edition: ";
 				getline(cin,input);
+				
 				books[key]->editEdition(capital(input));
 				cout<<endl<<"Edit Successful"<<endl;
 				break;
