6f786975eef09f62f9eac4943d044b6142c90e88
CoffeeStrike <omniexpansiontheory@gmail.com>
2025-03-18 19:56:38 +0800

    Add files via upload

--- Midterms Library System.cpp
+++ Midterms Library System.cpp
@@ -4,7 +4,6 @@
 #include <limits>
 using namespace std;
 
-
 bool isValidName(const string& name) {			//PROBABLY ADD AT PUBLISHER AND AUTHOR
     for (char ch : name) {
         if (!isalpha(ch) && ch != ' ') {
