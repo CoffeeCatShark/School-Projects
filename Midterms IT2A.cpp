3d68f08b49f60bc6f5812d5af10b6f9e0ae622c0
CoffeeStrike <omniexpansiontheory@gmail.com>
2025-04-04 13:36:11 +0800

    Add files via upload

--- Midterms IT2A.cpp
+++ Midterms IT2A.cpp
@@ -1,15 +1,16 @@
 using namespace std;
+#include <iostream>
 #include <string>
 #include <vector>
 
-Class Products{
+class Products{
 private: 
 int quantity;
 int productID;
 string name;
 float price;
 public:
-	Products(int _quantity, string _name, float _price, int _productID): quantity(_quantity), name(_name), price(_price), productID(_productID)
+	Products(int _quantity, string _name, float _price, int _productID): quantity(_quantity), name(_name), price(_price), productID(_productID) {}
 
 int getID(){
 	return productID;
@@ -27,10 +28,17 @@ float getPrice(){
 	return price;
 }
 
+void updateItem(){
+	
+}
+
+void updatePrice(){
+	
+}
 
 };
 
-Class Cart{
+class Cart{
 private:
 Products* products[100];
 int quantity;
@@ -41,3 +49,42 @@ public:
 	
 
 };
+
+void addProduct(){
+string input;
+int id,quantity;
+string name;
+float price;
+
+
+	cout<<"Product ID: ";
+	cin>>input;														//change to getline
+											//CONDITIONAL
+	id = stoi(input);
+
+	cout<<"Quantity: ";
+	cin>>input;
+											//CONDITIONAL
+	quantity = stoi(input);
+	
+	cout<<"Name: ";
+	getline(cin,input);
+	name = input;
+	
+	cout<<"Price: ";
+	getline(cin,input);
+											//CONDITIONAL
+											
+	price = stoi(input);
+
+	Products* product = new Products(quantity, name, price, id);
+	
+	
+}
+
+
+
+int main(){
+	
+	return 0;
+}
